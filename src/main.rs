db396a421ca571aea43b4c699a192df79a6aa1df
Glebanister <glebmar2001@gmail.com>
2020-12-03 13:21:28 +0300

    Add logic elements

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,32 @@
-mod command_line_interface;
-
-use command_line_interface::{CommandLineInterface, ExitHandler, InputHandler, StoppableInterface};
-use std::cell::RefCell;
-use std::cmp;
-use std::rc::Rc;
-
-struct Counter {
-    count: i32,
-    running: bool,
-}
-
-impl StoppableInterface for Counter {
-    fn stop(&mut self) {
-        self.running = false;
+mod logic {
+    pub enum Fact {
+        Implies(Box<Fact>, Box<Fact>),
+        And(Box<Fact>, Box<Fact>),
+        Or(Box<Fact>, Box<Fact>),
+        Not(Box<Fact>),
+        Atom(String),
     }
-    fn is_running(&self) -> bool {
-        return self.running;
+    pub type FactBox = Box<Fact>;
+    pub fn atom(name: &str) -> FactBox {
+        let name = String::from(name);
+        Box::new(Fact::Atom(name))
     }
-}
-
-impl Counter {
-    fn increase(&mut self) {
-        self.count += 1;
+    pub fn implies(a: FactBox, b: FactBox) -> FactBox {
+        Box::new(Fact::Implies(a, b))
     }
-
-    fn decrease(&mut self) {
-        self.count -= 1;
+    pub fn and(a: FactBox, b: FactBox) -> FactBox {
+        Box::new(Fact::And(a, b))
     }
-
-    fn get_count(&self) -> i32 {
-        return self.count;
+    pub fn or(a: FactBox, b: FactBox) -> FactBox {
+        Box::new(Fact::Or(a, b))
+    }
+    pub fn not(a: FactBox) -> FactBox {
+        Box::new(Fact::Not(a))
     }
 }
 
-struct IncreaseHandler {
-    counter: Rc<RefCell<Counter>>,
-}
-
-impl InputHandler<Counter> for IncreaseHandler {
-    fn handle(&self, input: &String) -> Option<String> {
-        let plus = String::from("++");
-        match input.cmp(&plus) {
-            cmp::Ordering::Equal => {
-                (*self.counter.borrow_mut()).increase();
-                return Some(String::new());
-            }
-            _ => return None,
-        };
-    }
-}
-
-struct DecreaseHandler {
-    counter: Rc<RefCell<Counter>>,
-}
-
-impl InputHandler<Counter> for DecreaseHandler {
-    fn handle(&self, input: &String) -> Option<String> {
-        let plus = String::from("--");
-        match input.cmp(&plus) {
-            cmp::Ordering::Equal => {
-                (*self.counter.borrow_mut()).decrease();
-                return Some(String::new());
-            }
-            _ => return None,
-        };
-    }
-}
-
-struct GetValueHandler {
-    counter: Rc<RefCell<Counter>>,
-}
-
-impl InputHandler<Counter> for GetValueHandler {
-    fn handle(&self, input: &String) -> Option<String> {
-        let plus = String::from("??");
-        match input.cmp(&plus) {
-            cmp::Ordering::Equal => {
-                return Some((*self.counter.borrow_mut()).get_count().to_string());
-            }
-            _ => return None,
-        };
-    }
-}
+use logic::*;
 
 fn main() {
-    let running = true;
-    let counter = Rc::new(RefCell::new(Counter { count: 0, running }));
-    let counter_interface = CommandLineInterface::new(
-        String::from(">> "),
-        String::from("Welcome to counter!"),
-        vec![
-            Box::new(GetValueHandler {
-                counter: counter.clone(),
-            }),
-            Box::new(IncreaseHandler {
-                counter: counter.clone(),
-            }),
-            Box::new(DecreaseHandler {
-                counter: counter.clone(),
-            }),
-            Box::new(ExitHandler {
-                interface: counter.clone(),
-            }),
-        ],
-        counter,
-    );
-    counter_interface.run();
+    let x = implies(atom("It is raining"), atom("I should get an umbrella"));
 }
